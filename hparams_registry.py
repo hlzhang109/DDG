bf59e0b3175e5ee7f468863b6a063122044d11a8
Yi-Fan Zhang <yifanzhang.cs@gmail.com>
2022-03-17 00:16:26 +0800

    Update hparams_registry.py

--- hparams_registry.py
+++ hparams_registry.py
@@ -84,6 +84,7 @@ def _hparams(algorithm, dataset, random_seed, stage):
         _hparam('sd_reg', 0.1, lambda r: 10**r.uniform(-5, -1))
 
     if 'DDG' in algorithm:
+        _hparam('is_ddg', True, lambda r: True)
         if 'MNIST' in dataset:
             print('mnsit')
             _hparam('steps', 10000, lambda r: 10000)
